ef13726df8e9be65e5cc6166a8db733656ca3ba3
Chicken C. Chicken <chickenchicken938427@gmail.com>
2023-04-02 14:01:19 +0200

    pills have random bodies

--- src/pill.rs
+++ src/pill.rs
@@ -2,10 +2,13 @@ use crate::actions::Actions;
 use crate::loading::TextureAssets;
 use crate::GameState;
 use bevy::input::mouse::MouseButtonInput;
+use bevy::math::cubic_splines::Point;
 use bevy::prelude::*;
 use bevy::window::PrimaryWindow;
 use bevy::window::Window;
 use bevy_rapier2d::prelude::*;
+use rand::distributions::{Distribution, Standard};
+use rand::{rngs::StdRng, SeedableRng};
 
 pub struct PillPlugin;
 
@@ -63,21 +66,47 @@ fn spawn_pills(
     textures: Res<TextureAssets>,
     mut ev_spawn_pill: EventReader<SpawnPillEvent>,
 ) {
+    let mut rng = StdRng::from_entropy();
     for ev in ev_spawn_pill.iter() {
+        let mut points = Vec::new();
+
+        let (x_scale, y_scale) = (150., 300.);
+        for _ in 0..10 {
+            let x: f32 = Standard.sample(&mut rng);
+            let y: f32 = Standard.sample(&mut rng);
+            points.push(Vect::new(
+                x * x_scale - x_scale / 2.,
+                y * y_scale - y_scale / 2.,
+            ));
+        }
+
+        let force_scale = 3000.;
+        let torque_scale = 20.;
+
+        let x_force_sample: f32 = Standard.sample(&mut rng);
+        let y_force_sample: f32 = Standard.sample(&mut rng);
+        let torque_sample: f32 = Standard.sample(&mut rng);
+
+        let x_force = x_force_sample * force_scale - force_scale / 2.;
+        let y_force = y_force_sample * force_scale - force_scale / 2.;
+        let torque_force = torque_sample * torque_scale - torque_scale / 2.;
+
         commands
             .spawn(SpriteBundle {
                 texture: textures.folder.get("textures/pill_0.png").unwrap().clone(),
-                transform: Transform::from_translation(Vec3::new(ev.0.x, ev.0.y, 1.)),
+                transform: Transform::from_translation(Vec3::new(ev.0.x, ev.0.y, 1.))
+                    .with_scale(Vec3::new(0.5, 0.5, 1.)),
                 ..Default::default()
             })
             .insert(Pill)
             .insert(RigidBody::Dynamic)
-            .insert(Collider::ball(60.0))
-            .insert(ColliderMassProperties::Density(2.0))
-            .insert(Restitution::coefficient(0.7))
+            // .insert(Collider::ball(60.0))
+            .insert(Collider::convex_hull(&points).unwrap())
+            .insert(ColliderMassProperties::Density(1.))
+            .insert(Restitution::coefficient(0.9))
             .insert(ExternalForce {
-                force: Vec2::new(100.0, 200.0),
-                torque: 14.0,
+                force: Vec2::new(x_force, y_force),
+                torque: torque_force,
             });
     }
 }
