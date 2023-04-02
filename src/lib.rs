3f351e017b3bb879acfff352ae64be39d657ab51
chickenchicken9 <127390746+chickenchicken9@users.noreply.github.com>
2023-04-02 08:43:36 +0000

    add physics ball

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,28 @@ use crate::loading::LoadingPlugin;
 use crate::menu::MenuPlugin;
 use crate::patient::PatientPlugin;
 use crate::player::PlayerPlugin;
+use bevy::prelude::*;
+use bevy_rapier2d::prelude::*;
+
+fn setup_physics(mut commands: Commands) {
+    /* Create the ground. */
+    commands
+        .spawn(Collider::cuboid(500.0, 50.0))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, -100.0, 0.0)));
+
+    /* Create the bouncing ball. */
+    commands
+        .spawn(RigidBody::Dynamic)
+        .insert(Collider::ball(50.0))
+        .insert(Restitution::coefficient(0.7))
+        .insert(TransformBundle::from(Transform::from_xyz(0.0, 400.0, 0.0)));
+}
+
+fn print_ball_altitude(positions: Query<&Transform, With<RigidBody>>) {
+    for transform in positions.iter() {
+        println!("Ball altitude: {}", transform.translation.y);
+    }
+}
 
 use bevy::app::App;
 #[cfg(debug_assertions)]
@@ -41,7 +63,12 @@ impl Plugin for GamePlugin {
             .add_plugin(ActionsPlugin)
             .add_plugin(InternalAudioPlugin)
             .add_plugin(PlayerPlugin)
-            .add_plugin(PatientPlugin);
+            .add_plugin(PatientPlugin)
+            // .add_plugins(DefaultPlugins)
+            .add_plugin(RapierPhysicsPlugin::<NoUserData>::pixels_per_meter(100.0))
+            .add_plugin(RapierDebugRenderPlugin::default())
+            .add_startup_system(setup_physics)
+            .add_system(print_ball_altitude);
 
         #[cfg(debug_assertions)]
         {
